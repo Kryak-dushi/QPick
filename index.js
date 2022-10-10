4c42902e8f84fe084009f53adfd20aa1fcfa773f
Kryak-dushi <fssashadrmatgat@gmail.com>
2022-10-10 15:00:40 +0400

    Added goods arrays

---
+++ index.js
@@ -0,0 +1,58 @@
+const headphones = [
+    {
+        img: "./assets/Apple BYZ S8521.png",
+        title: "Apple BYZ S8521",
+        price: 2927,
+        rate: 4.7,
+    },
+    {
+        img: "./assets/Apple EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+    {
+        img: "./assets/Apple EarPods Case.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+    {
+        img: "./assets/Apple BYZ S8521.png",
+        title: "Apple BYZ S8521",
+        price: 2927,
+        rate: 4.7,
+    },
+    {
+        img: "./assets/Apple EarPods.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+    {
+        img: "./assets/Apple EarPods Case.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate: 4.5,
+    },
+]
+const wireless_headphones = [
+    {
+        img: "./assets/Apple AirPods.png",
+        title: "Apple AirPods",
+        price: 9527,
+        rate: 4.7,
+    },
+    {
+        img: ".assets/GERLAX GH-04.png",
+        title: "Apple AirPods",
+        price: 9527,
+        rate: 4.7,
+    },
+    {
+        img: ".assets/BOROFONE BO4.png",
+        title: "Apple AirPods",
+        price: 9527,
+        rate: 4.7,
+    },
+]
